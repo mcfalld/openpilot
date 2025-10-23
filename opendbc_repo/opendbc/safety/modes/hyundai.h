4ae6602952fa2158accc11e091365d467db3c7bb
Derrick <mcfall.derrick@gmail.com>
2025-10-23 11:13:56 -0500

    Update Hyundai steering torque limits for improved performance

--- opendbc_repo/opendbc/safety/modes/hyundai.h
+++ opendbc_repo/opendbc/safety/modes/hyundai.h
@@ -7,8 +7,8 @@
   .max_torque = (steer), \
   .max_rate_up = (rate_up), \
   .max_rate_down = (rate_down), \
-  .max_rt_delta = 112, \
-  .driver_torque_allowance = 50, \
+  .max_rt_delta = 130, \
+  .driver_torque_allowance = 80, \
   .driver_torque_multiplier = 2, \
   .type = TorqueDriverLimited, \
    /* the EPS faults when the steering angle is above a certain threshold for too long. to prevent this, */ \
@@ -204,8 +204,8 @@ static void hyundai_rx_hook(const CANPacket_t *to_push) {
 
 static bool hyundai_tx_hook(const CANPacket_t *to_send) {
   const TorqueSteeringLimits HYUNDAI_STEERING_LIMITS = HYUNDAI_LIMITS(384, 3, 7);
-  const TorqueSteeringLimits HYUNDAI_STEERING_LIMITS_ALT = HYUNDAI_LIMITS(270, 2, 3);
-  const TorqueSteeringLimits HYUNDAI_STEERING_LIMITS_ALT_2 = HYUNDAI_LIMITS(170, 2, 3);
+  const TorqueSteeringLimits HYUNDAI_STEERING_LIMITS_ALT = HYUNDAI_LIMITS(320, 3, 4);
+  const TorqueSteeringLimits HYUNDAI_STEERING_LIMITS_ALT_2 = HYUNDAI_LIMITS(200, 2, 3);
 
   bool tx = true;
   int addr = GET_ADDR(to_send);
--- opendbc_repo/opendbc/safety/modes/hyundai_canfd.h
+++ opendbc_repo/opendbc/safety/modes/hyundai_canfd.h
@@ -144,11 +144,11 @@ static void hyundai_canfd_rx_hook(const CANPacket_t *to_push) {
 
 static bool hyundai_canfd_tx_hook(const CANPacket_t *to_send) {
   const TorqueSteeringLimits HYUNDAI_CANFD_STEERING_LIMITS = {
-    .max_torque = 270,
-    .max_rt_delta = 112,
-    .max_rate_up = 2,
-    .max_rate_down = 3,
-    .driver_torque_allowance = 250,
+    .max_torque = 360,
+    .max_rt_delta = 130,
+    .max_rate_up = 3,
+    .max_rate_down = 4,
+    .driver_torque_allowance = 280,
     .driver_torque_multiplier = 2,
     .type = TorqueDriverLimited,
 
